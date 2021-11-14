f337e95768dfdefa74cf83ee0484ddefaf28c762
xux <12345678>
2021-11-14 22:49:17 +0800

    项目创建准备阶段——判断当前目录是否为空功能开发

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -3,6 +3,7 @@
 
 const Command = require('@heis-cli-dev/command')
 const log = require('@heis-cli-dev/log')
+const fs = require('fs')
 
 function init(argv)  {
     // console.log('init', programName, cmdObj.force, process.env.CLI_TARGET_PATH)
@@ -17,7 +18,35 @@ class InitCommand extends Command {
         log.verbose('force', this.force)
     }
     execute() {
-        console.log('init的业务逻辑')
+        try { // 1.准备阶段
+            this.prepare()
+            // 2.下载模板
+            // 3.安装模板
+        } catch (e) {
+            log.error(e.message)
+        }
+    }
+
+    prepare() {
+        // 1.判断当前目录是否为空
+
+        if(!this.ifDirIsEmpty()) {
+            // 1.1询问是否继续创建
+        }
+        console.log(ret)
+        // 2.是否开启强制更新
+        // 3.选择创建项目或组件
+        // 4.获取项目的基本信息
+    }
+
+    ifDirIsEmpty() {
+        const localPath = process.cwd()
+        let fileList = fs.readdirSync(localPath)
+        // 文件过滤逻辑
+        fileList = fileList.filter(file => (
+            !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+        ))
+        return !fileList || fileList.length <= 0
     }
 }
 
