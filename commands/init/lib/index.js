5e1ac92841af930d8c928c4deaf20b5f0a6d7a73
xux <12345678>
2021-11-16 22:18:11 +0800

    自定义模板上线

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -169,7 +169,29 @@ class InitCommand extends Command {
     }
 
     async installCustomTemplate() {
-        console.log('安装自定义模板')
+        // 查询自定义模板的入口文件
+       if (await this.templateNpm.exists()) {
+           const rootFile = this.templateNpm.getRootFilePath()
+           if (fs.existsSync(rootFile)) {
+               log.notice('开始执行自定义模板')
+               const templatePath = path.resolve(this.templateNpm.cacheFilePath, 'template');
+               const options = {
+                   templateInfo: this.templateInfo,
+                   projectInfo: this.projectInfo,
+                   sourcePath: templatePath,
+                   targetPath: process.cwd(),
+               };
+               const code = `require('${rootFile}')(${JSON.stringify(options)})`
+               log.verbose('code', code)
+               await execAsync('node', ['-e', code], {
+                   stdio: 'inherit',
+                   cwd: process.cwd()
+               })
+               log.success('自定义模板安装成功')
+           } else {
+               throw new Error('自定义模板入口文件不存在')
+           }
+       }
     }
 
     async downloadTemplate() {
